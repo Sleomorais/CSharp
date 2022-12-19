5e77c6c240189df64884ab5ad9d46477439438e9
Leonardo Morais <leonardo.morais7778@gmail.com>
2022-12-19 17:20:13 -0300

    Exercicio de pedidos

---
+++ Exercicios/Pedido/Pedido/Program.cs
@@ -0,0 +1,52 @@
+﻿using Pedido.Entities;
+using Pedido.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace Pedido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Client Data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date(DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client c1 = new Client(name,email, date);
+            Order order = new Order(DateTime.Now,status,c1);
+
+            Console.WriteLine();
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product Name: ");
+                string pname = Console.ReadLine();
+                Console.Write("Product Price: ");
+                double price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                Product product = new Product(pname, price);
+
+                Console.Write("Quantity: ");
+                int quant = int.Parse(Console.ReadLine());
+
+                OrderItem item = new OrderItem(quant, price, product);
+
+                order.AddItem(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine(order);
+
+        }
+    }
+}
