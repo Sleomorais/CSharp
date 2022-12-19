e54a39641aec0c888bf1c94277f1bd7bd55eab74
Leonardo Morais <leonardo.morais7778@gmail.com>
2022-12-19 15:31:56 -0300

    Postagem Enumerações

---
+++ Exemplos/Postagem/Postagem/Program.cs
@@ -0,0 +1,37 @@
+﻿using Postagem.Entities;
+using System;
+
+namespace Postagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awsome!");
+
+            Post p1 = new Post(
+                 DateTime.Parse("21/06/2018 13:05:44"),
+                 "Traveling to new zeland",
+                 "I'm going to visit this wonderful country!",
+                 12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good Night");
+            Comment c4 = new Comment("Very Good");
+
+            Post p2 = new Post(
+                 DateTime.Parse("28/07/2018 23:14:19"),
+                 "Good Night",
+                 "See you tomorrow",
+                 5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+        }
+    }
+}
