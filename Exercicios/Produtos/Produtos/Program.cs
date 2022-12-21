3826a5410feeea740b3124244ec269689e97243c
Leonardo Morais <leonardo.morais7778@gmail.com>
2022-12-21 09:32:07 -0300

    Exercicio Produtos com Herança

---
+++ Exercicios/Produtos/Produtos/Program.cs
@@ -0,0 +1,55 @@
+﻿using Produtos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Common, used or imported (c/u/i)?: ");
+                char result = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                if(result == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (result == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else if(result == 'c')
+                {
+                    products.Add(new Product(name, price));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid data");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Price Tags");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
